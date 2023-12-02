c3cabe030013669ca3aac144a62f8934e2e88bba
Hannah Rose <52452654+HannahRose578@users.noreply.github.com>
2023-12-02 12:19:55 +0000

    Update part2.py

--- Day 2/part2.py
+++ Day 2/part2.py
@@ -1,6 +1,5 @@
 import re
 
-# Function to parse the input string into a structured format
 def parse_input(input_data):
     games = {}
     lines = input_data.strip().split('\n')
@@ -20,7 +19,6 @@ def parse_input(input_data):
 
     return games
 
-# Function to find the minimum set of cubes for each game and calculate its power
 def find_min_set_and_power(game_data):
     min_set = {"red": 0, "green": 0, "blue": 0}
 
@@ -30,20 +28,15 @@ def find_min_set_and_power(game_data):
     power = min_set["red"] * min_set["green"] * min_set["blue"]
     return power
 
-# Main function to process the input file and solve the puzzle
 def solve_puzzle_part_two(input_file):
     with open(input_file, 'r') as file:
         input_data = file.read()
 
     games_data = parse_input(input_data)
     
-    # Summing the powers of the minimum sets of all games
     sum_of_powers = sum(find_min_set_and_power(game_data) for game_data in games_data.values())
     return sum_of_powers
 
-# Path to the input file
 input_file_path = 'input2.txt'
-
-# Solve the puzzle for Part Two
 result_part_two = solve_puzzle_part_two(input_file_path)
-print(f"The sum of the power of the minimum sets is: {result_part_two}")
+print(result_part_two)
